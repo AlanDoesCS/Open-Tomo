25e93018489ac349f3f6ccdf8f4a957cc9f10e4c
Alan Smith <95879019+AlanDoesCS@users.noreply.github.com>
2025-06-04 15:12:26 +0200

    Add File Structure

--- src/main/java/io/github/AlanDoesCS/Main.java
+++ src/main/java/io/github/AlanDoesCS/Main.java
@@ -2,6 +2,6 @@ package io.github.AlanDoesCS;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Open Tomo is alive!");
+        System.out.println("Welcome to OpenTomo!");
     }
 }
